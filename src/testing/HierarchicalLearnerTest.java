805d404db99032ca7f1dd26b817369f3a69f897a
mlanden <mlanden@umbc.edu>
2017-06-28 11:25:30 -0400

    AMDP plans on no nav abstract

--- src/testing/HierarchicalLearnerTest.java
+++ src/testing/HierarchicalLearnerTest.java
@@ -89,7 +89,7 @@ public class HierarchicalLearnerTest {
 		double maxDelta = 0.01;
 		
 		TaxiState s = TaxiStateFactory.createClassicState();
-		Task RAMDProot = TaxiHierarchy.createAMDPHierarchy(correctMoveprob, fickleProb);
+		Task RAMDProot = TaxiHierarchy.createAMDPHierarchy(correctMoveprob, fickleProb, false);
 		OOSADomain base = TaxiHierarchy.getBaseDomain();
 //		Task RMAXQroot = TaxiHierarchy.createRMAXQHierarchy(correctMoveprob, fickleProb);
 		
