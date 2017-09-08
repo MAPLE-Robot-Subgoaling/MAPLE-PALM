bdc56dfb494f1428d630abbd6d9982bab7cb84ce
JW <jwinder1@umbc.edu>
2017-09-08 11:40:19 -0400

    Fix reward structure in config file

--- src/config/rmax/RmaxConfig.java
+++ src/config/rmax/RmaxConfig.java
@@ -3,7 +3,7 @@ package config.rmax;
 import burlap.behavior.singleagent.auxiliary.performance.PerformanceMetric;
 
 public class RmaxConfig {
-    public int vmax;
+    public double vmax;
     public int threshold;
     public double max_delta;
     public double max_delta_in_model;
